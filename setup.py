191cd1ff3823dcc7a8b7b76cbcf120d5440e492a
William <willtheorangeguy@outlook.com>
2016-04-25 18:00:40 -0700

    Main & Setup Files

---
+++ setup.py
@@ -0,0 +1,15 @@
+﻿import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32' : base = 'Win32GUI'
+
+opts = { 'include_files' : ['lotto.gif'] , 'includes' : ['re'] }
+
+setup(
+    name = 'Lotto' ,
+    version = '1.0' ,
+    description = 'Lottery Number Picker' ,
+    author = 'Mike McGrath' ,
+    options = {'build_exe' : opts } ,
+    executables = [ Executable( 'lotto.py' , base = base ) ] )
